abb04cc9accf858e53d4b1598d57dee057c69b12
Mica :) <103139051+micaelazocola@users.noreply.github.com>
2022-09-13 18:02:50 -0300

    src 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. holaaaaaa 
         </p>
         <a
           className="App-link"
@@ -18,8 +21,12 @@ function App() {
           Learn React
         </a>
       </header>
+      
+      
+      
     </div>
   );
 }
+
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+
+
+function Navbar () {
+    return (
+        <div className="header-nav">
+            <div className="logo-nav">
+               eCommerce 
+            </div>
+            <nav className="menu-navegador">
+                <ul className="menu-items">
+                    <li className="ul-item">Home</li>
+                    <li className="ul-item">About Us</li>
+                    <li className="ul-item">Shop</li>
+                    <li className="ul-item">Contact</li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
+
+export default Navbar
